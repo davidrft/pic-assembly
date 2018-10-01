debc5326392381152085dd245b6f2daba72d4ffb
David Riff <davidriff@outlook.com>
2018-10-01 04:36:01 -0300

    Adjust indentatiton

--- projeto1/projeto1.asm
+++ projeto1/projeto1.asm
@@ -72,124 +72,124 @@ DELAY_1S    equ     D'246'
 ; --- PROGRAMA PRINCIPAL ---
 main:
     bank1
-    movlw   B'00001111'     
-    movwf   TRISA           ; 1 - input, 0 - ouput
-    movlw   B'00001111'
-    movwf   TRISB           ; MSB   ...    LSB
-    movlw   B'10110001'     
-    movwf   OPTION_REG      ; define opcoes de operacao prescaler 1:4
+    movlw       B'00001111'     
+    movwf       TRISA           ; 1 - input, 0 - ouput
+    movlw       B'00001111'
+    movwf       TRISB           ; MSB   ...    LSB
+    movlw       B'10110001'     
+    movwf       OPTION_REG      ; define opcoes de operacao prescaler 1:4
 
-    movlw   B'00000000'
-    movwf   INTCON          ; define opcoes de interrupcao
+    movlw      B'00000000'
+    movwf      INTCON          ; define opcoes de interrupcao
 
-    movlw   B'00001000'
-    movwf   PCON            ; utilizar cristal interno de 4MHz
+    movlw      B'00001000'
+    movwf      PCON            ; utilizar cristal interno de 4MHz
     bank0
 
-    movlw   B'00000111'
-    movwf   CMCON           ; entradas analogicas desativadas
+    movlw      B'00000111'
+    movwf      CMCON           ; entradas analogicas desativadas
 
-    movlw   B'00000000'
-    movwf   PORTB           ; inicia outputs em 0
-    movlw   B'00000000'
-    movwf   PORTA           ; inicia outputs em 0
+    movlw      B'00000000'
+    movwf      PORTB           ; inicia outputs em 0
+    movlw      B'00000000'
+    movwf      PORTA           ; inicia outputs em 0
 
-	movlw	D'60'
-	movwf	contador1		; inicializa contador do sensor de presenca1
-	movlw	D'60'
-	movwf	contador2		; inicializa contador do sensor de presenca2
-	movlw	D'60'
-	movwf	contador3		; inicializa contador do sensor de presenca3
-    movlw	D'10'
-	movwf	contador_fumaca	; inicializa contador do sensor de fumaca
-	movlw	D'0'
-	movwf	presenca_ativa
-	clrf    TMR0
-    bcf     tflag
-    movlw   DELAY_1S
-    movwf   TMR0
+	movlw  	   D'60'
+	movwf	   contador1		; inicializa contador do sensor de presenca1
+	movlw	   D'60'
+	movwf	   contador2		; inicializa contador do sensor de presenca2
+	movlw	   D'60'
+	movwf	   contador3		; inicializa contador do sensor de presenca3
+    movlw	   D'10'
+	movwf	   contador_fumaca	; inicializa contador do sensor de fumaca
+	movlw	   D'0'
+	movwf	   presenca_ativa
+	clrf       TMR0
+    bcf        tflag
+    movlw      DELAY_1S
+    movwf      TMR0
 start:
-    call    check_presenca
-;    btfsc   fumaca
-;    call    tratar_fumaca
-    btfsc	tflag
-    call    contador
-    goto start
+    call       check_presenca
+;   btfsc      fumaca
+;   call       tratar_fumaca
+    btfsc	   tflag
+    call       contador
+    goto       start
 
 check_presenca:
-    btfss   presenca1               ; checa sensores da secao 1
-    goto	check2
-	btfss   luminosidade1
-    goto check2
-	bsf     presenca_ativa, ativo1   ; ativa contagem (liga a luz) da secao 1
-	bsf		luz1
+    btfss      presenca1               ; checa sensores da secao 1
+    goto   	   check2
+	btfss      luminosidade1
+    goto       check2
+	bsf        presenca_ativa,ativo1   ; ativa contagem (liga a luz) da secao 1
+	bsf		   luz1
 check2:
-	btfss   presenca2               ; checa sensores da secao 2
-	goto 	check3
-	btfss   luminosidade2
-	goto	check3
-	bsf     presenca_ativa, ativo2  ; ativa contagem (liga a luz) da secao 2
-	bsf		luz2
+	btfss      presenca2               ; checa sensores da secao 2
+	goto 	   check3
+	btfss      luminosidade2
+	goto	   check3
+	bsf        presenca_ativa,ativo2  ; ativa contagem (liga a luz) da secao 2
+	bsf		   luz2
 check3:  
-	btfss   presenca3              ; checa sensores da secao 3
-    goto	end_check
-	btfss   luminosidade3
-    goto	end_check
-	bsf     presenca_ativa, ativo3  ; ativa contagem (liga a luz) da secao 3
-	bsf		luz3
+	btfss      presenca3              ; checa sensores da secao 3
+    goto       end_check
+	btfss      luminosidade3
+    goto       end_check
+	bsf        presenca_ativa,ativo3  ; ativa contagem (liga a luz) da secao 3
+	bsf		   luz3
 end_check:
     return
 
 tratar_fumaca:
-    bsf presenca_ativa, ativa_f
+    bsf        presenca_ativa,ativa_f
     return
 
 contador:
-    bcf     tflag
+    bcf        tflag
 test_ativo1:
-	btfsc	presenca_ativa, ativo1
-    decfsz  contador1, 1
-    goto    test_ativo2
-    call    reset_cont1
+	btfsc      presenca_ativa,ativo1
+    decfsz     contador1,1
+    goto       test_ativo2
+    call       reset_cont1
 test_ativo2:
-	btfsc	presenca_ativa, ativo2
-    decfsz  contador2, 1
-    goto    test_ativo3
-    call    reset_cont2
+	btfsc	   presenca_ativa,ativo2
+    decfsz     contador2,1
+    goto       test_ativo3
+    call       reset_cont2
 test_ativo3:
-	btfsc	presenca_ativa, ativo3
-    decfsz  contador3, 1
-    goto    test_fumaca
-    call    reset_cont3
+	btfsc      presenca_ativa, ativo3
+    decfsz     contador3, 1
+    goto       test_fumaca
+    call       reset_cont3
 test_fumaca:
-    goto    end_fumaca  ;testar demais funcionalidades antes de implementar fumaca
-    btfsc   fumaca
-    decfsz  contador_fumaca
-    goto    end_fumaca
-    btfss   presenca_ativa, ativa_f
-    bsf     presenca_ativa, ativa_f
-    movlw   D'10'
-    movwf   contador_fumaca
+    goto       end_fumaca  ;testar demais funcionalidades antes de implementar fumaca
+    btfsc      fumaca
+    decfsz     contador_fumaca
+    goto       end_fumaca
+    btfss      presenca_ativa, ativa_f
+    bsf        presenca_ativa, ativa_f
+    movlw      D'10'
+    movwf      contador_fumaca
 end_fumaca:
     return  
 
 reset_cont1:
-    movlw 	D'60'
-    movwf 	contador1
-    bcf 	presenca_ativa, ativo1
-    bcf 	luz1
+    movlw      D'60'
+    movwf 	   contador1
+    bcf 	   presenca_ativa, ativo1
+    bcf 	   luz1
     return
 reset_cont2:
-    movlw	 D'60'
-    movwf 	contador2
-    bcf 	presenca_ativa, ativo2
-    bcf 	luz2
+    movlw      D'60'
+    movwf 	   contador2
+    bcf 	   presenca_ativa, ativo2
+    bcf 	   luz2
     return
 reset_cont3:
-    movlw 	D'60'
-    movwf 	contador3
-    bcf 	presenca_ativa, ativo3
-    bcf 	luz3
+    movlw      D'60'
+    movwf 	   contador3
+    bcf 	   presenca_ativa, ativo3
+    bcf 	   luz3
     return
 
 end
